58ac17fc5f20e8244212d0bbdb3bba8f17f10c88
ansaralam322 <125534761+ansaralam322@users.noreply.github.com>
2023-02-16 10:56:25 +0530

    Add files via upload

---
+++ R/Ansaralam.R
@@ -0,0 +1,13 @@
+#' Create Histogram Boxplot and Numeric Summary
+#' @export
+#' @param x numeric variable
+
+kun<-function(x){
+  par(mfrow=c(1,2))
+  hist(x,col=rainbow(30))
+  boxplot(x,col="blue")
+  par(mfrow=c(1,1))
+  data.frame(min=min(x),
+             median=median(x),
+             max=max(x))
+}
